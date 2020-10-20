813e02c140e76e3a266509e0c8e959a17b8cb849
Putra Utama <nyoman.utama@amarbank.co.id>
2020-10-20 20:41:49 +0700

    feat(restaurant-app): add restaurant use case

---
+++ lib/domain/usecase/get_list_restaurant_usecase.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/foundation.dart';
+import 'package:restaurant_app/domain/entity/restaurant_entity.dart';
+import 'package:restaurant_app/domain/repository/restaurant_repository.dart';
+
+abstract class GetListRestaurantUseCase {
+  Future<List<RestaurantEntity>> getListRestaurant();
+}
+
+class GetListRestaurantUseCaseImpl extends GetListRestaurantUseCase {
+  RestaurantRepository restaurantRepository;
+
+  GetListRestaurantUseCaseImpl({@required this.restaurantRepository});
+
+  @override
+  Future<List<RestaurantEntity>> getListRestaurant() =>
+      restaurantRepository.getListRestaurant();
+}
