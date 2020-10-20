f232b1147d9f0ca5ca21a26ff2ed37130636e9c5
Putra Utama <nyoman.utama@amarbank.co.id>
2020-10-20 22:04:38 +0700

    feat(restaurant-app): add detail restaurant screen tab view

---
+++ lib/presentation/screen/detail_restaurant_screen/foods_screen.dart
@@ -0,0 +1,20 @@
+part of 'detail_restaurant_screen.dart';
+
+class FoodsScreen extends StatelessWidget {
+  final List<FoodsEntity> foods;
+
+  FoodsScreen({this.foods});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: foods.length,
+        shrinkWrap: true,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsets.fromLTRB(16.w, 0.w, 16.w, 16.w),
+            child: Text(foods[index].name,style: TextStyle(fontSize: 20.sp,color: CustomColors.darkGrey),),
+          );
+        });
+  }
+}
