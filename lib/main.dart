ecc30b140397c5e60042ecaee56e910ca9f32f49
Putra Utama <nyoman.utama@amarbank.co.id>
2020-10-20 23:59:10 +0700

    feat(restaurant-app): add remove debuhg checker to false

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Restaurant App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.orange,
         visualDensity: VisualDensity.adaptivePlatformDensity,
