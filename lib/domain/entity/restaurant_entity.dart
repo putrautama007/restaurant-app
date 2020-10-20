46cb221d7ee5d3467512ba556dfcc9a596894274
Putra Utama <nyoman.utama@amarbank.co.id>
2020-10-20 20:41:10 +0700

    feat(restaurant-app): add restaurant entity

---
+++ lib/domain/entity/restaurant_entity.dart
@@ -0,0 +1,61 @@
+import 'package:equatable/equatable.dart';
+
+class RestaurantListEntity extends Equatable {
+  final List<RestaurantEntity> restaurants;
+
+  RestaurantListEntity({this.restaurants});
+
+  @override
+  List<Object> get props => [restaurants];
+}
+
+class RestaurantEntity extends Equatable {
+  final String id;
+  final String name;
+  final String description;
+  final String pictureId;
+  final String city;
+  final String rating;
+  final MenusEntity menus;
+
+  RestaurantEntity(
+      {this.id,
+      this.name,
+      this.description,
+      this.pictureId,
+      this.city,
+      this.rating,
+      this.menus});
+
+  @override
+  List<Object> get props =>
+      [id, name, description, pictureId, city, rating, menus];
+}
+
+class MenusEntity extends Equatable {
+  final List<FoodsEntity> foods;
+  final List<DrinksEntity> drinks;
+
+  MenusEntity({this.foods, this.drinks});
+
+  @override
+  List<Object> get props => [foods, drinks];
+}
+
+class FoodsEntity extends Equatable {
+  final String name;
+
+  FoodsEntity({this.name});
+
+  @override
+  List<Object> get props => [name];
+}
+
+class DrinksEntity extends Equatable {
+  final String name;
+
+  DrinksEntity({this.name});
+
+  @override
+  List<Object> get props => [name];
+}
