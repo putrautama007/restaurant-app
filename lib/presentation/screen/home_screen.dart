19493cb2473abbb04bcf5ab94d6b62bc53ae8cd8
Putra Utama <nyoman.utama@amarbank.co.id>
2020-10-20 16:19:19 +0700

    feat(restaurant-app): add home screen

---
+++ lib/presentation/screen/home_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant_app/external/custom_colors.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: CustomColors.lightYellow,
+    );
+  }
+}
