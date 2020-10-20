a80072066265b2de21421dc96745a6bfaee61abd
Putra Utama <nyoman.utama@amarbank.co.id>
2020-10-20 16:40:07 +0700

    feat(restaurant-app): implement splash bloc on splash screen

--- lib/presentation/screen/splash_screen.dart
+++ lib/presentation/screen/splash_screen.dart
@@ -1,22 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:restaurant_app/external/custom_colors.dart';
 import 'package:restaurant_app/external/custom_screen_utils.dart';
 import 'package:restaurant_app/external/image_strings.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:restaurant_app/presentation/bloc/splash_bloc/splash_bloc.dart';
+import 'package:restaurant_app/presentation/screen/home_screen.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     CustomScreenUtils.initScreenUtils(context);
-    return Scaffold(
-      backgroundColor: CustomColors.white,
-      body: Center(
-        child: Image.asset(
-          ImageStrings.logo,
-          width: 400.w,
-          height: 400.w,
-          fit: BoxFit.cover,
-        ),
+    return BlocProvider(
+      create: (context) => SplashBloc()..add(LoadSplashScreenEvent()),
+      child: BlocListener<SplashBloc, SplashState>(
+        listener: (context, state) {
+          if (state is SplashLoadedState) {
+            Future.delayed(Duration(seconds: 2)).then((_) => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => HomeScreen())));
+          }
+        },
+        child: BlocBuilder<SplashBloc, SplashState>(builder: (context, state) {
+          return Scaffold(
+            backgroundColor: CustomColors.white,
+            body: Center(
+              child: Image.asset(
+                ImageStrings.logo,
+                width: 400.w,
+                height: 400.w,
+                fit: BoxFit.cover,
+              ),
+            ),
+          );
+        }),
       ),
     );
   }
