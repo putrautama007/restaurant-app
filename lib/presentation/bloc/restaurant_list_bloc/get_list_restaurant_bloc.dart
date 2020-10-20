1200d83100a33d330712f067a78c77422cb827b4
Putra Utama <nyoman.utama@amarbank.co.id>
2020-10-20 20:41:59 +0700

    feat(restaurant-app): add restaurant bloc

---
+++ lib/presentation/bloc/restaurant_list_bloc/get_list_restaurant_bloc.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:restaurant_app/domain/entity/restaurant_entity.dart';
+import 'package:restaurant_app/domain/usecase/get_list_restaurant_usecase.dart';
+
+part 'get_list_restaurant_event.dart';
+
+part 'get_list_restaurant_state.dart';
+
+class GetListRestaurantBloc
+    extends Bloc<GetListRestaurantEvent, GetListRestaurantState> {
+  GetListRestaurantUseCase getListRestaurantUseCase;
+
+  GetListRestaurantBloc({this.getListRestaurantUseCase})
+      : super(GetListRestaurantInitialState());
+
+  @override
+  Stream<GetListRestaurantState> mapEventToState(
+      GetListRestaurantEvent event) async* {
+    if (event is GetListRestaurant) {
+      yield GetListRestaurantLoadingState();
+      var listRestaurant = await getListRestaurantUseCase.getListRestaurant();
+      yield GetListRestaurantLoadedState(listRestaurant: listRestaurant);
+    }
+  }
+}
---
+++ lib/presentation/bloc/restaurant_list_bloc/get_list_restaurant_event.dart
@@ -0,0 +1,10 @@
+part of 'get_list_restaurant_bloc.dart';
+
+abstract class GetListRestaurantEvent extends Equatable {
+  const GetListRestaurantEvent();
+}
+
+class GetListRestaurant extends GetListRestaurantEvent {
+  @override
+  List<Object> get props => [];
+}
