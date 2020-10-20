5a8253aead7320f922053476e4d625d29458226c
Putra Utama <nyoman.utama@amarbank.co.id>
2020-10-20 16:39:52 +0700

    feat(restaurant-app): add splash bloc

---
+++ lib/presentation/bloc/splash_bloc/splash_state.dart
@@ -0,0 +1,15 @@
+part of 'splash_bloc.dart';
+
+abstract class SplashState extends Equatable {
+  const SplashState();
+}
+
+class SplashInitialState extends SplashState {
+  @override
+  List<Object> get props => [];
+}
+
+class SplashLoadedState extends SplashState {
+  @override
+  List<Object> get props => [];
+}
