2f197d88981f8cfd8d0d02aab02ab3480a90dc7a
Putra Utama <nyoman.utama@amarbank.co.id>
2020-10-20 20:42:16 +0700

    feat(restaurant-app): implement restaurant bloc to home screen

--- lib/presentation/screen/home_screen.dart
+++ lib/presentation/screen/home_screen.dart
@@ -1,11 +1,198 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:restaurant_app/data/local/datasource/local_data_source.dart';
+import 'package:restaurant_app/data/local/repository/restaurant_repository_impl.dart';
+import 'package:restaurant_app/domain/usecase/get_list_restaurant_usecase.dart';
 import 'package:restaurant_app/external/custom_colors.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:restaurant_app/external/custom_screen_utils.dart';
+import 'package:restaurant_app/presentation/bloc/restaurant_list_bloc/get_list_restaurant_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: CustomColors.lightYellow,
+    CustomScreenUtils.initScreenUtils(context);
+    return BlocProvider(
+      create: (context) => GetListRestaurantBloc(
+          getListRestaurantUseCase: GetListRestaurantUseCaseImpl(
+              restaurantRepository: RestaurantRepositoryIml(
+                  localDataSource: LocalDataSourceImpl())))
+        ..add(GetListRestaurant()),
+      child: Scaffold(
+        backgroundColor: CustomColors.yellow,
+        appBar: AppBar(
+          backgroundColor: CustomColors.yellow,
+          elevation: 0.0,
+          centerTitle: false,
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                "Restaurant",
+                style: TextStyle(color: CustomColors.white, fontSize: 20.sp),
+              ),
+              Text(
+                "Recommendation restaurant for you!",
+                style: TextStyle(color: CustomColors.white, fontSize: 12.sp),
+              )
+            ],
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.search,
+                color: CustomColors.white,
+              ),
+            ),
+          ],
+        ),
+        body: BlocBuilder<GetListRestaurantBloc, GetListRestaurantState>(
+          builder: (context, state) {
+            if (state is GetListRestaurantLoadedState) {
+              return SingleChildScrollView(
+                child: Container(
+                    margin: EdgeInsets.only(top: 16.w),
+                    padding: EdgeInsets.all(16.w),
+                    decoration: BoxDecoration(
+                      color: CustomColors.lightYellow,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(40.0),
+                        topRight: Radius.circular(40.0),
+                      ),
+                    ),
+                    child: ListView.builder(
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        itemCount: state.listRestaurant.length,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            margin: EdgeInsets.fromLTRB(8.w, 8.w, 8.w, 0.w),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(15.0),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey,
+                                  offset: Offset(0.0, 0.5),
+                                  blurRadius: 1.0,
+                                ),
+                              ],
+                            ),
+                            child: InkWell(
+                              onTap: () {},
+                              child: Row(
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(15.0),
+                                    child: Image.network(
+                                      state.listRestaurant[index].pictureId,
+                                      height: 90.w,
+                                      width: 125.w,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Padding(
+                                      padding: EdgeInsets.fromLTRB(
+                                          16.w, 0.w, 16.w, 0.w),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        mainAxisSize: MainAxisSize.max,
+                                        children: [
+                                          Text(
+                                            state.listRestaurant[index].name,
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                color: CustomColors.darkGrey,
+                                                fontSize: 16.sp),
+                                          ),
+                                          Padding(
+                                            padding: EdgeInsets.only(top: 8.w),
+                                            child: Row(
+                                              mainAxisAlignment:
+                                              MainAxisAlignment.start,
+                                              crossAxisAlignment:
+                                              CrossAxisAlignment.center,
+                                              children: [
+                                                Icon(
+                                                  Icons.pin_drop,
+                                                  color: CustomColors.grey,
+                                                  size: 16.w,
+                                                ),
+                                                Padding(
+                                                  padding: EdgeInsets.only(
+                                                      left: 8.w),
+                                                  child: Text(
+                                                      "${state.listRestaurant[index].city}",
+                                                      style: TextStyle(
+                                                          fontWeight:
+                                                          FontWeight.normal,
+                                                          color: CustomColors.darkGrey,
+                                                          fontSize: 14.sp)),
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: EdgeInsets.only(top: 8.w),
+                                            child: Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.start,
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.center,
+                                              children: [
+                                                Icon(
+                                                  Icons.star,
+                                                  color: Colors.amber,
+                                                  size: 16.w,
+                                                ),
+                                                Padding(
+                                                  padding: EdgeInsets.only(
+                                                      left: 8.w),
+                                                  child: Text(
+                                                      "${state.listRestaurant[index].rating}",
+                                                      style: TextStyle(
+                                                          fontWeight:
+                                                              FontWeight.normal,
+                                                          color: CustomColors.darkGrey,
+                                                          fontSize: 14.sp)),
+                                                ),
+                                              ],
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          );
+                        })),
+              );
+            } else {
+              return Container(
+                padding: EdgeInsets.all(16.w),
+                decoration: BoxDecoration(
+                  color: CustomColors.lightYellow,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40.0),
+                    topRight: Radius.circular(40.0),
+                  ),
+                ),
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            }
+          },
+        ),
+      ),
     );
   }
 }
