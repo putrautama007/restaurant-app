7bc568523b22b0b39ea0ede3ef191ba06a0c4fcc
Putra Utama <nyoman.utama@amarbank.co.id>
2020-10-21 12:06:28 +0700

    refactor(restaurant-app): change api url

--- lib/data/remote/datasource/api_constant.dart
+++ lib/data/remote/datasource/api_constant.dart
@@ -1,9 +1,9 @@
 class ApiConstant {
-  static const String baseUrl = "https://restaurant-api.dicoding.dev/";
-  static const String listRestaurant = "$baseUrl/list";
-  static const String detailRestaurant = "$baseUrl/detail/";
-  static const String searchRestaurant = "$baseUrl/search?q=";
-  static const String reviewRestaurant = "$baseUrl//review";
+  static const String baseUrl = "https://restaurant-api.dicoding.dev";
+  static const String listRestaurant = "/list";
+  static const String detailRestaurant = "/detail/";
+  static const String searchRestaurant = "/search?q=";
+  static const String reviewRestaurant = "/review";
   static const String smallImageResolution = "$baseUrl/images/small/";
   static const String mediumImageResolution = "$baseUrl/images/medium/";
 }
