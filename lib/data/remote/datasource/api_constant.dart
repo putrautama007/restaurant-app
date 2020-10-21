765cd43b55eace0029d6c11a780cc8f72a645358
Putra Utama <nyoman.utama@amarbank.co.id>
2020-10-21 11:05:11 +0700

    feat(restaurant-app): add api constant

---
+++ lib/data/remote/datasource/api_constant.dart
@@ -0,0 +1,8 @@
+class ApiConstant {
+  static const String baseUrl = "https://restaurant-api.dicoding.dev/";
+  static const String listRestaurant = "$baseUrl/list";
+  static const String detailRestaurant = "$baseUrl/detail/";
+  static const String searchRestaurant = "$baseUrl/search?q=";
+  static const String smallImageResolution = "$baseUrl/images/small/";
+  static const String mediumImageResolution = "$baseUrl/images/medium/";
+}
