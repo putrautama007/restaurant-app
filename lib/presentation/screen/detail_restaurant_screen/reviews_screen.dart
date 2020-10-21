9b1b592bf84b56d91b96bbc67715374cf2366062
Putra Utama <nyoman.utama@amarbank.co.id>
2020-10-21 21:18:16 +0700

    feat(restaurant-app): add bottom margin on consumer review

--- lib/presentation/screen/detail_restaurant_screen/reviews_screen.dart
+++ lib/presentation/screen/detail_restaurant_screen/reviews_screen.dart
@@ -19,46 +19,49 @@ class _ReviewsScreenState extends State<ReviewsScreen> {
       color: CustomColors.lightYellow,
       child: Stack(
         children: [
-          ListView.builder(
-              itemCount: widget.consumerReviews.length,
-              shrinkWrap: true,
-              itemBuilder: (context, index) {
-                return Container(
-                  margin: EdgeInsets.fromLTRB(8.w, 0.w, 8.w, 16.w),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(15.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey,
-                        offset: Offset(0.0, 0.5),
-                        blurRadius: 1.0,
-                      ),
-                    ],
-                  ),
-                  padding: EdgeInsets.fromLTRB(16.w, 16.w, 16.w, 16.w),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(widget.consumerReviews[index].name,
-                          style: TextStyle(
-                              fontSize: 20.sp,
-                              color: CustomColors.darkGrey,
-                              fontWeight: FontWeight.bold)),
-                      Text(widget.consumerReviews[index].review,
-                          style: TextStyle(
-                              fontSize: 16.sp,
-                              color: CustomColors.darkGrey,
-                              fontWeight: FontWeight.normal)),
-                      Text(widget.consumerReviews[index].date,
-                          style: TextStyle(
-                              fontSize: 14.sp,
-                              color: CustomColors.grey,
-                              fontWeight: FontWeight.normal)),
-                    ],
-                  ),
-                );
-              }),
+          Container(
+            margin: EdgeInsets.only(bottom: 32.w),
+            child: ListView.builder(
+                itemCount: widget.consumerReviews.length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: EdgeInsets.fromLTRB(8.w, 0.w, 8.w, 16.w),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(0.0, 0.5),
+                          blurRadius: 1.0,
+                        ),
+                      ],
+                    ),
+                    padding: EdgeInsets.fromLTRB(16.w, 16.w, 16.w, 16.w),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(widget.consumerReviews[index].name,
+                            style: TextStyle(
+                                fontSize: 20.sp,
+                                color: CustomColors.darkGrey,
+                                fontWeight: FontWeight.bold)),
+                        Text(widget.consumerReviews[index].review,
+                            style: TextStyle(
+                                fontSize: 16.sp,
+                                color: CustomColors.darkGrey,
+                                fontWeight: FontWeight.normal)),
+                        Text(widget.consumerReviews[index].date,
+                            style: TextStyle(
+                                fontSize: 14.sp,
+                                color: CustomColors.grey,
+                                fontWeight: FontWeight.normal)),
+                      ],
+                    ),
+                  );
+                }),
+          ),
           Align(
             alignment: Alignment.bottomCenter,
             child: CustomButton(
