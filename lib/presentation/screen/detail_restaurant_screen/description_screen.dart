05b6c50cd45f05628378672da803ef4b66bdd2be
Putra Utama <nyoman.utama@amarbank.co.id>
2020-10-21 19:56:28 +0700

    feat(restaurant-app): add category on detail restaurant

--- lib/presentation/screen/detail_restaurant_screen/description_screen.dart
+++ lib/presentation/screen/detail_restaurant_screen/description_screen.dart
@@ -9,8 +9,10 @@ class DescriptionScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: CustomColors.lightYellow,
-      padding: EdgeInsets.only(left:16.w,right: 16.w),
+      padding: EdgeInsets.only(left: 16.w, right: 16.w),
       child: ListView(
+        physics: NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
         children: [
           Row(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -29,23 +31,69 @@ class DescriptionScreen extends StatelessWidget {
                         color: CustomColors.darkGrey,
                         fontSize: 18.sp)),
               ),
-              Padding(
-                padding: EdgeInsets.only(left: 8.w),
-                child: Icon(
-                  Icons.pin_drop,
-                  color: CustomColors.grey,
-                  size: 20.w,
-                ),
+            ],
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Icon(
+                Icons.pin_drop,
+                color: CustomColors.grey,
+                size: 20.w,
               ),
               Padding(
                 padding: EdgeInsets.only(left: 8.w),
-                child: Text(restaurantEntity.city,
+                child: Text(
+                    "${restaurantEntity.address}, ${restaurantEntity.city}",
                     style: TextStyle(
                         fontWeight: FontWeight.normal,
                         color: CustomColors.darkGrey,
-                        fontSize: 18.sp)),
+                        fontSize: 16.sp)),
               ),
             ],
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 16.w),
+            child: Text(
+              "Category",
+              style: TextStyle(
+                  color: CustomColors.yellow,
+                  fontSize: 24.sp,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          Container(
+            height: 20.w,
+            margin: EdgeInsets.only(top: 8.w),
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                itemCount: restaurantEntity.categories.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: EdgeInsets.only(right: 8.w),
+                    padding: EdgeInsets.fromLTRB(16.w,0.w,16.w,0.w),
+                    decoration: BoxDecoration(
+                      color: CustomColors.yellow,
+                      borderRadius: BorderRadius.circular(15.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(0.0, 0.5),
+                          blurRadius: 1.0,
+                        ),
+                      ],
+                    ),
+                    child: Text(
+                      restaurantEntity.categories[index].name,
+                      style: TextStyle(
+                          fontSize: 16.sp,
+                          color: CustomColors.white,
+                          fontWeight: FontWeight.normal),
+                    ),
+                  );
+                }),
           ),
           Padding(
             padding: EdgeInsets.only(top: 16.w),
