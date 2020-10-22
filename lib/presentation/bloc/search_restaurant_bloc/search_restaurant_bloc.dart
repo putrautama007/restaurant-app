e80a011004d48d992e2180afbed52d03174112d8
Putra Utama <nyoman.utama@amarbank.co.id>
2020-10-23 00:10:44 +0700

    refactor(restaurant-app): refactor

--- lib/presentation/bloc/search_restaurant_bloc/search_restaurant_bloc.dart
+++ lib/presentation/bloc/search_restaurant_bloc/search_restaurant_bloc.dart
@@ -1,7 +1,7 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:restaurant_app/domain/entity/restaurant_entity.dart';
-import 'package:restaurant_app/domain/usecase/serch_restaurant_usecase.dart';
+import 'package:restaurant_app/domain/usecase/search_restaurant_usecase.dart';
 
 part 'search_restaurant_event.dart';
 
